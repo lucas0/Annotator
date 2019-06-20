44ee365a9669923beac9cb4a6ec0d0541d00fa7f
lucas0 <lucas.ccomp@gmail.com>
2019-06-20 12:11:12 +0100

    wipe

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-cwd = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-	FLASK_DEBUG=1,
-	SECRET_KEY = '4ZuS2a\BH=44sk!$'
-	SQLALCHEMY_DATABASE_URI = "sqlite:///"+os.path.join(cwd,"app.db")
-	SQLALCHEMY_TRACK_MODIFICATIONS = False

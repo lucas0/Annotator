5af37777f55f0a929f1251f52bb4e0cab557a269
lucas0 <lucas.ccomp@gmail.com>
2019-06-21 16:18:01 +0100

    annotator running reading from HTML.csv

--- annotator/core/views.py
+++ annotator/core/views.py
@@ -18,6 +18,8 @@ import requests
 
 cwd = os.path.abspath(__file__+"/..")
 datadir = os.path.abspath(cwd+"/../data")
+html_dir = os.path.abspath(cwd+"/../../html_getter")
+samples_path = html_dir+"/samples_html_no_err.csv"
 
 chrome_options = Options()
 chrome_options.add_argument("--headless")
@@ -30,6 +32,7 @@ browser = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome
 
 res_header = ["page", "claim", "verdict", "tags", "date", "author", "source_list", "source_url", "value", "name"]
 samples_path = datadir+"/samples.csv"
+samples_path = html_dir+"/samples_html_no_err.csv"
 results_path = datadir+"/results/"
 count_path = datadir+"/count.csv"
 # AUX FUNCTIONS
@@ -45,6 +48,7 @@ def save_annotation(page, origin, value, name):
     s_p = pd.read_csv(samples_path, sep='\t', encoding="latin1")
     entry = s_p.loc[(s_p["page"] == page) & (s_p["source_url"] == origin)]
     if not (entry.empty):
+        entry = entry.drop(columns=['page_html','source_html'])
         n_entry = entry.values.tolist()[0]
         n_entry.extend([value, name])
         results_filename = results_path+name+".csv"
@@ -54,6 +58,8 @@ def save_annotation(page, origin, value, name):
             results = pd.DataFrame(columns=res_header)
         oldEntry = results.loc[(results["page"] == page) & (results["source_url"] == origin)]
         if oldEntry.empty:
+            print(n_entry)
+            print(len(n_entry))
             results.loc[len(results)] = n_entry
         results.to_csv(results_filename, sep=',', index=False)
         # keeps track of how many times page was annotated
@@ -73,7 +79,7 @@ def get_least_annotated_page(name,aPage=None):
     print("done: ", len(done_by_annotator), " | total: ", len(s_p))
     
     if len(done_by_annotator) == len(s_p.page.unique()):
-        return "Last annotation done! Thank you!", "Last annotation done! Thank you!", "Last annotation done! Thank you!", "Last annotation done! Thank you!"
+        return "Last annotation done! Thank you!", None, None, None, None, None, None, None
 
     #Creates or reads countfile:
     if os.path.exists(count_path):
@@ -105,22 +111,16 @@ def get_least_annotated_page(name,aPage=None):
         
     entry = remOrigins.iloc[0]
     entry = s_p[(s_p.page.isin([entry.page]) & s_p.source_url.isin([entry.source_url]))].iloc[0]
-    # entry = s_p.loc[s_p['page']==entry['page'] && s_p['source_url']==entry['source_url']][0]
-    
+    a_page = entry.page.strip()
+    o_page = entry.source_url.strip()
+    a_html = entry.page_html.strip()
+    o_html = entry.source_html.strip()
     src_lst = entry.source_list.strip()
-    totalPage = len(s_p.loc[s_p['page'] == entry.page])
-    donePage = totalPage - len(remOrigins)
-    # Check if origin is broken link or not
-    try:
-        o_status = requests.get(entry.source_url.strip(), headers={'User-Agent': 'a user agent'}).status_code
-    except requests.exceptions.RequestException as e:
-        return entry.page.strip(), entry.source_url.strip(), src_lst, donePage, totalPage,len(done_by_annotator)
-    
-    if o_status == requests.codes.ok:
-        return entry.page.strip(), entry.source_url.strip(), src_lst, donePage, totalPage, len(done_by_annotator)
-    else:
-        save_annotation(entry.page.strip(), entry.source_url.strip(), '3', name)
-        return get_least_annotated_page(name,entry.page.strip())
+
+    a_total = len(s_p.loc[s_p['page'] == entry.page])
+    a_done  = a_total - len(remOrigins)
+
+    return a_page, o_page, a_html, o_html, src_lst, a_done, a_total, len(done_by_annotator)
 
 # VIEWS
 def home(request):
@@ -143,91 +143,28 @@ def home(request):
 			if op:
 				if op in ['1', '2', '3', '4']:
 					save_annotation(session.get('claim'),session.get('origin'), op, name)
-					a_url, o_url, src_lst, a_done, a_total, t_done = get_least_annotated_page(name, session.get('claim'))
+					a_url, o_url, a_html, o_html, src_lst, a_done, a_total, t_done = get_least_annotated_page(name, session.get('claim'))
 					# Turn string representation of a list to a list
 					src_lst = ast.literal_eval(src_lst)
 			else:
 				a_url = session.get('claim')
 				o_url = session.get('origin')
+				a_html = session.get('a_html')
+				o_html = session.get('o_html')
 				src_lst = session.get('src_lst')
 				a_done = session.get('a_done')
 				a_total = session.get('a_total')
 				t_done = session.get('t_done')
 		else:
-			a_url, o_url, src_lst, a_done, a_total, t_done = get_least_annotated_page(name)
+			a_url, o_url, a_html, o_html, src_lst, a_done, a_total, t_done = get_least_annotated_page(name)
 			# Turn string representation of a list to a list
 			src_lst = ast.literal_eval(src_lst)
 		
-		print("GETTING ARTICLE WITH SELENIUM")
-		try:
-			browser.get(a_url)
-		except TimeoutException as e:
-			return redirect('home')
-
-		a_html = browser.page_source
-
-		print("DISABLING LINKS")
-		#Disable non-origin links
-		soup = BeautifulSoup(a_html, 'lxml')
-		for a in soup.findAll('a'):
-			if a.has_attr("href"):
-				if not (a['href'] in src_lst):
-					del a['href']
-					a.name='span'
-				else:
-					a['id']=a['href']
-
-		if len(soup.findAll('span')) > 0:
-			soup.span.unwrap()
-
-		print("REMOVING TAGS FROM A")
-		# Remove all tags inside <a> tags, if any
-		for a in soup.findAll('a'):
-			words=a.get_text()
-			a.clear()
-			a.insert(1,words)
-
-		decomposers = [s for s in soup.find_all(["span","div"]) if "Snopes Needs Your Help" in s.text]
-		parents = []
-		[parents.extend(s.find_parents('w-div')) for s in decomposers]
-		[p.decompose() for p in parents if p is not None]
-
-		a_html=str(soup)
-
-		print("INJECTING JS SCRIPTS")
-		#Add code to higlight hyperlink of current origin and scroll to it
-		injectionPoint=a_html.split("</body>")
-		highlightFnc = '<script> function highlight(){ let link = parent.document.getElementById("oLink").href; document.getElementById(link).style.backgroundColor="yellow"; } if (window.attachEvent) {window.attachEvent("onload", highlight);} else if (window.addEventListener) {window.addEventListener("load", highlight, false);} else {document.addEventListener("load", highlight, false);} </script>'
-		highlightLnkFnc = '<script> function highlightLnk(newLnk,oldLnk){document.getElementById(oldLnk).style.backgroundColor="white"; document.getElementById(newLnk).style.backgroundColor="yellow"; } </script></body>'
-		scrollFnc = '<script> function scrollDown(){ let link = parent.document.getElementById("oLink").href; var elmnt = document.getElementById(link); elmnt.scrollIntoView({ behavior: "smooth", block: "start"  }); } if (window.attachEvent) {window.attachEvent("onload", scrollDown);} else if (window.addEventListener) {window.addEventListener("load", scrollDown, false);} else {document.addEventListener("load", scrollDown, false);} </script>'
-		a_html=injectionPoint[0]+highlightFnc+scrollFnc+highlightLnkFnc+injectionPoint[1]
-
-		print("INJECTING JS SCRIPTS")
-		#Add JQuery CDN and event-handler
-		injectionPoint=a_html.split("</body>")
-		jqueryCode='<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>'
-		jqEvnt = '<script> $("a").on("click", function(e){ e.preventDefault(); if(e.target.href){ if(!(e.target.href == parent.document.getElementById("oLink").href)){ parent.document.getElementById("oframe").srcdoc="<p>LOADING..PLEASE WAIT</p>"; let link = e.target.href; let claim = parent.document.getElementById("cLink").href; $.ajax({ url: "/newOrigin", data: {"link":link, "claim":claim}, type: "POST", success: function(response) { if(response.st=="ok"){ parent.document.getElementById("oframe").srcdoc=response.site; parent.document.getElementById("oLink").href=response.link; highlightLnk(response.link, response.oldLnk); } else if (response.st=="bad"){ alert("Broken link :/");       parent.document.getElementById("oframe").srcdoc=response.site; } else{ alert("Link already annotated!");      parent.document.getElementById("oframe").srcdoc=response.site; } }, error:function(error) { console.log(error); } }); }}}); </script></body>'
-		a_html=injectionPoint[0]+jqueryCode+jqEvnt+injectionPoint[1]
-
-		print("GETTING SOURCE WITH SELENIUM")
-		browser.get(o_url)
-		o_html = browser.page_source
-
-		print("FIXING LOCALLY REFERENCED SRCs")
-		soup = BeautifulSoup(o_html, 'lxml')
-		domain = '{uri.scheme}://{uri.netloc}/'.format(uri=urlparse(o_url))
-		for elem in soup.findAll(['img', 'script']):
-			if elem.has_attr('src'):
-				src = elem['src']
-				if not src.startswith("http"):
-					src.lstrip("/")
-					elem['src'] = domain+src
-
-		o_html = soup.prettify()
-
 		#Save claim and origin links and list of origins in session
 		session['claim'] = a_url
 		session['origin'] = o_url
+		session['a_html'] = a_html
+		session['o_html'] = o_html
 		session['src_lst'] = src_lst
 		session['a_done'] = a_done
 		session['a_total'] = a_total

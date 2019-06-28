866e0408f0466c88671ae9b3095af9a79095e97a
Mohamed <mohamedmoustafaofficial@gmail.com>
2019-06-28 23:21:32 +0200

    will fix unfoolding

--- annotator/data/fileUnfold.py
+++ annotator/data/fileUnfold.py
@@ -12,6 +12,7 @@ cwd = os.path.abspath(__file__+"/..")
 
 snopes = open(cwd+"/folded_snopes.csv","r")
 output = open(cwd+"/samples.csv","a+")
+error_log = open(cwd+"/error_log.csv","a+")
 
 in_header = ["page", "claim", "verdict", "tags", "date", "author","source_list"]
 out_header = "page\tclaim\tverdict\ttags\tdate\tauthor\tsource_list\tsource_url\n"
@@ -20,9 +21,12 @@ output.write(out_header)
 in_data=snopes.readlines()
 
 for i in range(1,len(in_data)):
-    sourceList=ast.literal_eval(in_data[i].split("\t")[6])
-    for j in range(0,len(sourceList)):
-        output.write(in_data[i].rstrip()+"\t"+sourceList[j]+"\n")
+	try:
+		sourceList=ast.literal_eval(in_data[i].split("\t")[6])
+		for j in range(0,len(sourceList)):
+		output.write(in_data[i].rstrip()+"\t"+sourceList[j]+"\n")
+	except:
+		error_log.write(in_data[i])	
 
 snopes.close()
 output.close()

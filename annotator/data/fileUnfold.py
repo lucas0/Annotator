dd17df11c9cf92679eff56f55f6a630b59e4f9e7
Mohamed Moustafa <MohamedMoustafaOfficial@gmail.com>
2019-06-29 20:52:09 +0100

    fixed saving into csv

--- annotator/data/fileUnfold.py
+++ annotator/data/fileUnfold.py
@@ -13,23 +13,19 @@ cwd = os.path.abspath(__file__+"/..")
 snopes = pd.read_csv(cwd+"/folded_snopes.csv", sep='\t', encoding="latin1")
 output_path = cwd+"/samples.csv"
 
-in_header = ["page", "claim", "verdict", "tags", "date", "author","source_list"]
 out_header = ["page", "claim", "verdict", "tags", "date", "author","source_list","source_url"]
-
+count = len(snopes)
 for idx, e in snopes.iterrows():
+    print("Row ",idx," out of ",count)
     entry = e.values.tolist()
-    print("entry")
-    print(entry)
-    print("entry")
     src_lst = ast.literal_eval(entry[6])
+
     for src in src_lst:
-        print("src")
-        print(src)
-        print("src")
-        n_entry = entry.extend([src])
+        n_entry = entry + [src]
         if os.path.exists(output_path):
             output = pd.read_csv(output_path, sep='\t', encoding="latin1")
         else:
             output = pd.DataFrame(columns=out_header)
+
         output.loc[len(output)] = n_entry
         output.to_csv(output_path, sep='\t', index=False)
